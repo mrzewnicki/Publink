52d1479a87606d38dd2ef098482f1503670228ed
Mateusz Rzewnicki <mateusz.rzewnicki@wp.pl>
2025-10-17 15:23:23 +0200

    Aggregation Models

---
+++ Server/Publink.Data/Repositories/Models/ChangeAggregate.cs
@@ -0,0 +1,10 @@
+﻿namespace Publink.Data.Repositories.Models;
+
+public class ChangeAggregate
+{
+    public Guid CorrelationId { get; set; }
+    public int Count { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public TimeSpan Duration => End - Start;
+}
---
+++ Server/Publink.Data/Repositories/Models/OrganisationItem.cs
@@ -0,0 +1,7 @@
+﻿namespace Publink.Data.Repositories.Models;
+
+public class OrganisationItem
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+}

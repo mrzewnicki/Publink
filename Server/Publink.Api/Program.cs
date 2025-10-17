fd2c7aea141f5a21ff227c5dfade77a648f350d2
Mateusz Rzewnicki <mateusz.rzewnicki@wp.pl>
2025-10-17 10:21:14 +0200

    Base structure and EF configuration

---
+++ Server/Publink.Api/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Publink.Data.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Configure DbContext with Npgsql provider and connection string from configuration
+var connectionString = builder.Configuration.GetConnectionString("PublinkConnection");
+builder.Services.AddDbContext<PublinkDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Server/Publink.Data/Context/PublinkDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Publink.Data.Context;
+
+public class PublinkDbContext : DbContext
+{
+    public PublinkDbContext(DbContextOptions<PublinkDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Configure your entities here when they are added
+    }
+}
